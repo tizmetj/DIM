7ae03687106d96a0f3b437d5e6987505f42ed483
Kyle Shay <kyle.shay@ymail.com>
2020-09-16 20:11:59 -0700

    Feature flag button move, undo filter items from sheet

--- src/app/inventory/StoreBucket.tsx
+++ src/app/inventory/StoreBucket.tsx
@@ -100,7 +100,8 @@ function StoreBucket({
     items.filter((i) => !i.equipped),
     itemSortOrder
   );
-  const hidePullFromBucket = $featureFlags.mobileCategoryStrip && isPhonePortrait;
+  const hidePullFromBucket =
+    $featureFlags.movePullFromButton && $featureFlags.mobileCategoryStrip && isPhonePortrait;
 
   return (
     <>
